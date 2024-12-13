87c07234bba1222b9f8f5765b96fad456de610c9
Baptiste Gogolewski <gogolewskibaptiste@gmail.com>
2024-12-13 11:53:11 +0100

    About section is done. There is a little description about this project and you can get in touch if you have issues

--- src/main/java/com/wrapper/controllers/MainWindowController.java
+++ src/main/java/com/wrapper/controllers/MainWindowController.java
@@ -1,4 +1,31 @@
 package com.wrapper.controllers;
 
-public class MainWindowController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class MainWindowController
+{
+    @FXML
+    private Hyperlink githubLink = new Hyperlink();
+
+    @FXML
+    public void onGithubLinkClicked()
+    {
+        String url = "https://github.com/Amiens-Engineering-School/virtualbox-wrapper";
+        githubLink.setOnAction(e -> {
+            try
+            {
+                Desktop.getDesktop().browse(new URI(url));
+            }
+            catch (IOException | URISyntaxException ex)
+            {
+                ex.printStackTrace();
+            }
+        });
+    }
 }
