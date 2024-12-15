924a2558be56516f00c4ad310d9ab11ef0dd1ae7
Baptiste Gogolewski <gogolewskibaptiste@gmail.com>
2024-12-15 21:22:56 +0100

    Added functionality for modifying an existing virtual machine (#3)

--- src/main/java/com/wrapper/App.java
+++ src/main/java/com/wrapper/App.java
@@ -1,5 +1,6 @@
 package com.wrapper;
 
+import com.wrapper.controllers.MainWindowController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -17,18 +18,21 @@ public class App extends Application {
 
     @Override
     public void start(@SuppressWarnings("exports") Stage stage) throws IOException {
-        scene = new Scene(loadFXML("mainWindow"), 640, 480);
+        FXMLLoader fxmlLoader = loadFXML("mainWindow");
+        scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
+
+        ((MainWindowController) fxmlLoader.getController()).initialize();
     }
 
     public static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
+        scene.setRoot(loadFXML(fxml).load());
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
+    private static FXMLLoader loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/wrapper/fxml/" + fxml + ".fxml"));
-        return fxmlLoader.load();
+        return fxmlLoader;
     }
 
     public static void main(String[] args) {
